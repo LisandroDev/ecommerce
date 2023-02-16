05a9a31d04ac6797c26239e9b1ab6a4e732002d8
lichaa824 <lisandrorubi@gmail.com>
2023-02-16 20:43:44 -0300

    Fix styling of navigation bar

--- ecommerce/src/components/Navigation/Navigation.tsx
+++ ecommerce/src/components/Navigation/Navigation.tsx
@@ -3,12 +3,12 @@ import './Navigation.css'
 
 const Navigation = () => {
     return (
-        <div className='container flex flex-wrap items-center justify-between mx-auto mt-5'>
+        <div className='container flex flex-wrap flex-row items-center justify-between md:justify-between lg:justify-between mx-auto mt-5'>
 
-            <div><div>Logo</div></div>
+            <div className='w-full md:w-auto flex justify-center'><div className='logo font-bold text-3xl'><span className='text-[#3C9379]'>N</span>eithaClothes</div></div>
 
-            <div className='hidden w-full md:block md:w-auto '>
-                <ul className='manrope-font font-medium text-base text-gray-500 flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
+            <div className='w-full flex justify-center md:block md:w-auto '>
+                <ul className=' manrope-font font-medium text-base text-gray-500 flex flex-row space-x-4 p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
                     <li>Home</li>
                     <li>Products</li>
                     <li>Categories</li>
@@ -16,7 +16,7 @@ const Navigation = () => {
                 </ul>
             </div>
 
-            <div>Cart</div>
+            <div className='w-full text-center basis-1/5 md:block md:w-auto'>Cart</div>
 
         </div>
     )
