40c1651647a57bd1ec3b7b6ced9c6a5c74e882d5
lichaa824 <lisandrorubi@gmail.com>
2023-03-04 16:22:45 -0300

    Initial backend setup

---
+++ ecommerce-backend/src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
