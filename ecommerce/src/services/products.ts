2690331db38a7268720860bceec5f93e2ec1e6aa
lichaa824 <lisandrorubi@gmail.com>
2023-03-07 20:06:34 -0300

    Add service that fetch products from backend

---
+++ ecommerce/src/services/products.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { Product } from "../types/product";
+
+const getProducts = async (limit?: number) => {
+  try {
+    const response = await axios.get(`http://localhost:3003/products`);
+    if (limit) {
+      return response.data.splice(0,limit);
+    } else {
+      return response.data;
+    }
+  } catch (error) {
+    throw new Error("No data was return.");
+  }
+};
+
+export { getProducts };
