a5957aa95847461f534eda159dc1e62eaa7de64a
lichaa824 <lisandrorubi@gmail.com>
2023-03-14 17:38:03 -0300

    Add home / landing component

---
+++ ecommerce/src/components/Home/Home.tsx
@@ -0,0 +1,17 @@
+import Brands from "../Brands/Brand";
+import Landing from "../Landing/Landing";
+import Reasons from "../Reasons/Reasons";
+import PopularProducts from "../PopularProducts/PopularProducts";
+
+
+
+const Home = () => {
+    return (
+        <div>      <Landing />
+            <Brands />
+            <PopularProducts />
+            <Reasons /></div>
+    )
+}
+
+export default Home
