179f06e32bdc5dd7e80a5ca18d21e8168d25f2bf
lichaa824 <lisandrorubi@gmail.com>
2023-03-14 18:34:48 -0300

    Add more images

--- ecommerce-backend/src/data/products.ts
+++ ecommerce-backend/src/data/products.ts
@@ -2,21 +2,21 @@ import {Product} from "../types"
 import { toNewProduct } from "../utils";
 
 const data = [
-  { id: 1, title: "Nike Air", price: 109.95, category: "shoes", imageUrl: "/images/nike_air" },
-  { id: 2, title: "Kiikii Shirt", price: 22, category: "shirt", imageUrl: "/images/kiiki_shirt" },
+  { id: 1, title: "Nike Air", price: 109.95, category: "shoes",imageUrl: "/images/airNike" },
+  { id: 2, title: "Kiikii Shirt", price: 22, category: "shirt", imageUrl: "/images/animeShirt"},
   {
     id: 3,
     title: "Leave the Road Shirt",
     price: 20,
     category: "shirt",
-    imageUrl: "/images/leavetheroad_shirt",
+    imageUrl: "/images/leaveTheRoadShirt"
   },
   {
     id: 4,
     title: "Adidas classic",
     price: 92,
     category: "shoes",
-    imageUrl: "/images/retro_shoes",
+    imageUrl: "/images/retroShoes"
   },
 ];
 
--- ecommerce-backend/src/routes/images.ts
+++ ecommerce-backend/src/routes/images.ts
@@ -3,20 +3,20 @@ const path = require('path')
 
 const router = express.Router();
 
-router.get("/nike_air", (_req, res) => {
-    res.sendFile(path.resolve('./src/public/resources/airNike.jpg'));
+router.get("/:name", (req, res) => {
+    res.sendFile(path.resolve(`./src/public/resources/${req.params['name']}.jpg`));
 });
 
-router.get("/kiiki_shirt", (_req, res) => {
-    res.sendFile(path.resolve('./src/public/resources/animeShirt.jpg'));
-});
+// router.get("/kiiki_shirt", (_req, res) => {
+//     res.sendFile(path.resolve('./src/public/resources/animeShirt.jpg'));
+// });
 
-router.get("/leavetheroad_shirt", (_req, res) => {
-    res.sendFile(path.resolve('./src/public/resources/leaveTheRoadShirt.jpg'));
-});
+// router.get("/leavetheroad_shirt", (_req, res) => {
+//     res.sendFile(path.resolve('./src/public/resources/leaveTheRoadShirt.jpg'));
+// });
 
-router.get("/retro_shoes", (_req, res) => {
-    res.sendFile(path.resolve('./src/public/resources/retroShoes.jpg'));
-});
+// router.get("/retro_shoes", (_req, res) => {
+//     res.sendFile(path.resolve('./src/public/resources/retroShoes.jpg'));
+// });
 
 export default router
