dda2fc6d139809bb087baf2b4e5aaa4ea47eea27
lichaa824 <lisandrorubi@gmail.com>
2023-03-14 18:01:31 -0300

    Route changes

--- ecommerce-backend/src/routes/images.ts
+++ ecommerce-backend/src/routes/images.ts
@@ -3,21 +3,20 @@ const path = require('path')
 
 const router = express.Router();
 
-
 router.get("/nike_air", (_req, res) => {
-    res.sendFile(path.resolve('./src/public/clothesphoto/airNike.jpg'));
+    res.sendFile(path.resolve('./src/public/resources/airNike.jpg'));
 });
 
 router.get("/kiiki_shirt", (_req, res) => {
-    res.sendFile(path.resolve('./src/public/clothesphoto/animeShirt.jpg'));
+    res.sendFile(path.resolve('./src/public/resources/animeShirt.jpg'));
 });
 
 router.get("/leavetheroad_shirt", (_req, res) => {
-    res.sendFile(path.resolve('./src/public/clothesphoto/leaveTheRoadShirt.jpg'));
+    res.sendFile(path.resolve('./src/public/resources/leaveTheRoadShirt.jpg'));
 });
 
 router.get("/retro_shoes", (_req, res) => {
-    res.sendFile(path.resolve('./src/public/clothesphoto/retroShoes.jpg'));
+    res.sendFile(path.resolve('./src/public/resources/retroShoes.jpg'));
 });
 
 export default router
