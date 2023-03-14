b5e30b075717eb77c8d950da1c471c20e8b2836b
lichaa824 <lisandrorubi@gmail.com>
2023-03-14 17:37:31 -0300

    Add footer

---
+++ ecommerce/src/components/Footer/Footer.tsx
@@ -0,0 +1,21 @@
+const Footer = () => {
+    return (
+        <footer className="flex mt-28 mr-8 ml-8 mb-8 justify-around">
+            <div >
+                <div className="logo font-bold text-base md:text-3xl">
+                    <span className="text-[#3C9379]">N</span>eithaClothes
+                </div>
+                <div className="mt-6 text-[#535A56] w-1/2 text-xs hidden md:block md:text-sm" >Neithaclothes is an online store dealing with all product
+                    Neithaclothes provide cheap, high quality, products to
+                    customers</div>
+                <div className="mt-6 text-[#535A56] w-1/2 text-xs  md:hidden md:text-sm">Shop affordable, high-quality products at Neithaclothes online!</div>
+            </div>
+            <div>
+                <div className="text-base font-extralight  md:text-xl lg:text-2xl manrope-font ">Connect Us</div>
+                <div className="text-[#535A56] text-xs md:text-sm"><p>support@neithaclothes.com</p><p>+1-202-555-0187</p><p>123 Fake Street</p></div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
