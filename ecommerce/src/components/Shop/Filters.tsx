469e2ff0a3f01cd1dc17c9ec002b7f8b951c0fe1
lichaa824 <lisandrorubi@gmail.com>
2023-04-04 14:35:56 -0300

    add filter functionality

---
+++ ecommerce/src/components/Shop/Filters.tsx
@@ -0,0 +1,46 @@
+
+type FilterProps = {
+  categories: Array<string>;
+  setfilters: Function;
+  filters: Array<string>;
+};
+
+const Filters = ({ categories, setfilters, filters }: FilterProps) => {
+
+
+    const handleChange = (categoryValue: string) => {
+        if(filters.includes(categoryValue)){
+            const newFilters = [...filters]
+            const index = filters.indexOf(categoryValue);
+            newFilters.splice(index, 1)
+            setfilters(newFilters);
+        } else {
+            const newFilters = [...filters]
+            newFilters.push(categoryValue)
+            setfilters(newFilters)
+        }
+        console.log(filters)
+    }
+
+  return (
+    <div className="manrope-font">
+      <ul>
+        {categories.map((category) => (
+          <li  key={category}>
+            <input
+              type="checkbox"
+              name={category}
+              className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+              value={category}
+              placeholder={category}
+              onChange={() => handleChange(category)}
+            />
+            <label className="ml-3 min-w-0 flex-1 text-gray-500">{category.charAt(0).toUpperCase() + category.slice(1)}</label>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Filters;
--- ecommerce/src/components/Shop/Shop.tsx
+++ ecommerce/src/components/Shop/Shop.tsx
@@ -1,18 +1,35 @@
 import { useEffect, useState } from "react";
 import { getProducts } from "../../services/products";
 import Products from "./Products";
+import Filters from "./Filters";
 import Pagination from "./Pagination";
+import { Product } from "../../types/product";
 
 const Shop = () => {
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
+  const [filters, setFilters] = useState([]);
+  const [categories, setCategories] = useState([""]);
   const [itemsPerPage] = useState(6);
+
+  // Get list of categories
+
+  const getCategorys = (data: Product[]) => {
+    const listOfCategories = data.map((e) => e.category);
+    const uniqueCategories: string[] = listOfCategories.filter(
+      (element, index) => {
+        return listOfCategories.indexOf(element) === index;
+      }
+    );
+    setCategories(uniqueCategories);
+  };
 
   useEffect(() => {
     const fetchItems = async () => {
       setLoading(true);
       const data = await getProducts();
+      getCategorys(data);
       setItems(data);
       setLoading(false);
     };
@@ -20,21 +37,40 @@ const Shop = () => {
     fetchItems();
   }, []);
 
-  //Get current items
+  //Pagination variables.
 
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = items.slice(indexOfFirstItem, indexOfLastItem);
+
+  const currentItems =
+    filters.length > 0
+      ? items
+          .filter((e) => filters.includes(e["category"]))
+          .slice(indexOfFirstItem, indexOfLastItem)
+      : items.slice(indexOfFirstItem, indexOfLastItem);
+
+  const totalItems =
+    filters.length > 0
+      ? items.filter((e) => filters.includes(e["category"])).length
+      : items.length;
 
   const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
   return (
-    <div className="flex justify-center">
+    <div className="flex sm:flex-row sm:flex-wrap md:flex-nowrap md:flex-row justify-center manrope-font">
+      <div className="mt-24">
+        <p>Filter by category: </p>
+        <Filters
+          categories={categories}
+          setfilters={setFilters}
+          filters={filters}
+        />
+      </div>
       <div className="flex justify-center gap-y-4 flex-col">
         <Products items={currentItems} loading={loading} />
         <Pagination
           itemsPerPage={itemsPerPage}
-          totalItems={items.length}
+          totalItems={totalItems}
           paginate={paginate}
           currentPage={currentPage}
         />
