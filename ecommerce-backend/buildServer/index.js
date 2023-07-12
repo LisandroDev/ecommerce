005edb7e819b927ff27ecd02340358735d2024c8
lichaa824 <lisandrorubi@gmail.com>
2023-07-12 10:00:08 -0300

    new build

--- ecommerce-backend/buildServer/index.js
+++ ecommerce-backend/buildServer/index.js
@@ -11,9 +11,9 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 const PORT = 3003;
-app.use(express_1.default.static('build'));
 app.use('/images', images_1.default);
 app.use('/products', products_1.default);
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+module.exports = app;
