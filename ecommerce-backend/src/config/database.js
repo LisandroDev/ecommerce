d89063ca36ff3550959e0f24df23051dcfeace50
lichaa824 <lisandrorubi@gmail.com>
2023-07-12 09:51:37 -0300

    add pg

--- ecommerce-backend/src/config/database.js
+++ ecommerce-backend/src/config/database.js
@@ -7,7 +7,8 @@ module.exports =
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "postgres"
+    "dialect": "postgres",
+     dialectModule: require('pg')
   },
   "test": {
     "username": "root",
